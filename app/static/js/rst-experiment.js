a170e0ef75d2e9b8a07c557f16887a3a3bcd22b8
J Branson Byers <jbransonbyers@gmail.com>
2020-07-01 13:19:38 -0400

    Added practice, and the instructions to go with it.

--- app/static/js/rst-experiment.js
+++ app/static/js/rst-experiment.js
@@ -23,7 +23,7 @@ const choice_duration = 10000;
 const feedback_duration = 2000;
 
 // Define missed repsonses count.
-var missed_threshold = 6;
+var missed_threshold = 100;
 var missed_responses = 0;
 
 //------------------------------------//
@@ -33,16 +33,41 @@ var missed_responses = 0;
 // Define image scaling CSS.
 const style = "width:auto; height:auto; max-width:100%; max-height:80vh;";
 
-var instructions = {
+var instructions1 = {
   type: 'instructions',
   pages: [
     `<img src='../static/img/instructions01.png' style="${style}"</img>`,
     `<img src='../static/img/instructions02.png' style="${style}"</img>`,
+    `<img src='../static/img/instructions03.png' style="${style}"</img>`,
+    `<img src='../static/img/instructions04.png' style="${style}"</img>`,
+    `<img src='../static/img/instructions05.png' style="${style}"</img>`,
+    `<img src='../static/img/instructions06.png' style="${style}"</img>`,
+    `<img src='../static/img/instructions07.png' style="${style}"</img>`,
   ],
   show_clickable_nav: true,
   button_label_previous: "Prev",
   button_label_next: "Next"
-}
+};
+
+var instructions2 = {
+  type: 'instructions',
+  pages: [
+    `<img src='../static/img/instructions08.png' style="${style}"</img>`,
+  ],
+  show_clickable_nav: true,
+  button_label_previous: "Prev",
+  button_label_next: "Next"
+};
+
+var instructions3 = {
+  type: 'instructions',
+  pages: [
+    `<img src='../static/img/instructions09.png' style="${style}"</img>`,
+  ],
+  show_clickable_nav: true,
+  button_label_previous: "Prev",
+  button_label_next: "Next"
+};
 
 //------------------------------------//
 // Define risk sensitivity task.
@@ -50,16 +75,15 @@ var instructions = {
 
 // Define task conditions.
 const conditions = [
-  [0, 1],    // certain-0 vs certain-1 fish
-  [1, 2],    // certain-1 vs certain-2 fish
-  [0, 3],    // certain-0 vs risky-2 fish
-  [1, 3],    // certain-1 vs risky-2 fish (1)
-  [1, 3],    // certain-1 vs risky-2 fish (2)
-  [2, 3],    // certain-2 vs risky-2 fish
-  [0, 4],    // forced certain-0
-  [1, 4],    // forced certain-1
-  [2, 4],    // forced certain-2
-  [3, 4],    // forced risky-2
+  [0, 1],    // (0) certain-0 vs certain-1 fish
+  [1, 2],    // (1) certain-1 vs certain-2 fish
+  [0, 3],    // (2) certain-0 vs risky-2 fish
+  [1, 3],    // (3) certain-1 vs risky-2 fish
+  [2, 3],    // (4) certain-2 vs risky-2 fish
+  [0, 4],    // (5) forced certain-0
+  [1, 4],    // (6) forced certain-1
+  [2, 4],    // (7) forced certain-2
+  [3, 4],    // (8) forced risky-2
 ];
 
 // Define correct key press.
@@ -76,71 +100,99 @@ const correct = [
   [37,39],
 ];
 
+// Define practice trials
+const practice = [];
+
 // Define trial sets.
-const set = [];
-for (i = 0; i < 10; i++) {
+// Creates a pair of trials for each condition (so that each context appears on the left and right)
+function create_trial_set(start_condition, end_condition){
+  var set = [];
 
-  for (j = 0; j < 2; j++) {
+  for (i = start_condition; (i < end_condition+1); i++) { // the conditions
 
-    // Define trial.
-    const trial = {
-      type: 'rst-trial',
-      beach_left: contexts[conditions[i][j]],
-      beach_right: contexts[conditions[i][1-j]],
-      outcome_left: jsPsych.randomization.repeat(outcomes[conditions[i][j]],1)[0],
-      outcome_right: jsPsych.randomization.repeat(outcomes[conditions[i][1-j]],1)[0],
-      choices: choices,
-      choice_duration: choice_duration,
-      feedback_duration: feedback_duration,
-      data: {
-        stimulus_L: conditions[i][j],
-        stimulus_R: conditions[i][1-j],
-        correct: correct[i][j]
-      },
-      on_finish: function(data) {
+    for (j = 0; j < 2; j++) { //two of each condition, flipping left and right
 
-        // Evaluate missing data
-        if ( data.key == null ) {
+      // Define trial.
+      const trial = {
+        type: 'rst-trial',
+        beach_left: contexts[conditions[i][j]],
+        beach_right: contexts[conditions[i][1-j]],
+        outcome_left: jsPsych.randomization.repeat(outcomes[conditions[i][j]],1)[0],
+        outcome_right: jsPsych.randomization.repeat(outcomes[conditions[i][1-j]],1)[0],
+        choices: choices,
+        choice_duration: choice_duration,
+        feedback_duration: feedback_duration,
+        data: {
+          stimulus_L: conditions[i][j],
+          stimulus_R: conditions[i][1-j],
+          correct: correct[i][j]
+        },
+        on_finish: function(data) {
 
-          // Set missing data to true.
-          data.missing = true;
+          // Evaluate missing data
+          if ( data.key == null ) {
 
-          // Increment counter. Check if experiment should end.
-          missed_responses++;
-          if (missed_responses >= missed_threshold) {
-            jsPsych.endExperiment();
+            // Set missing data to true.
+            data.missing = true;
+
+            // Increment counter. Check if experiment should end.
+            missed_responses++;
+            if (missed_responses >= missed_threshold) {
+              jsPsych.endExperiment();
+            }
+
+          } else {
+
+            // Set missing data to false.
+            data.missing = false;
+
+            // Define accuracy.
+            data.accuracy = data.key == data.correct;
+
           }
-
-        } else {
-
-          // Set missing data to false.
-          data.missing = false;
-
-          // Define accuracy.
-          data.accuracy = data.key == data.correct;
 
         }
 
       }
 
-    }
-
-    // Define looping node.
-    const trial_node = {
-      timeline: [trial],
-      loop_function: function(data) {
-        return data.values()[0].missing;
+      // Define looping node.
+      const trial_node = {
+        timeline: [trial],
+        loop_function: function(data) {
+          return data.values()[0].missing;
+        }
       }
+
+      // Append trial.
+      set.push(trial_node);
+
     }
-
-    // Append trial.
-    set.push(trial_node);
-
   }
+  return set;
+}//function
 
+// Create the first round of practice
+// It consists of 4 of each forced choice conditions.
+// The below adds a pair of each, twice (k=2)
+var forced_choice_practice = [];
+for (k = 0; k < 2; k++){
+  console.log(k);
+  forced_choice_practice = forced_choice_practice.concat(jsPsych.randomization.shuffle(create_trial_set(5,8)))
 }
 
+// Create the second round of practice
+// It consists of 10 trials.
+// 2 of each type of choice trials, of which there are 5 option.
+var choice_practice = [];
+for (k = 0; k < 2; k++){
+  console.log(k);
+  choice_practice = choice_practice.concat(jsPsych.randomization.shuffle(create_trial_set(0,4)))
+}
+
+const set = create_trial_set(0,8); //add all the conditions
+
 // Define task trials.
+// Add five of each pair of trials. 10 total for each condition.
 var rst_trials = [];
 for (i = 0; i < 5; i++) {
   rst_trials = rst_trials.concat(jsPsych.randomization.shuffle(set));
